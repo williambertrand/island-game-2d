c312241851af0a9e9c5a3584cad58afc5da185e8
William <william.bertrand94@gmail.com>
2021-02-13 08:38:50 -0700

    initial commit: player and camera movement done

---
+++ Assets/Scripts/CameraFollowPlayer.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraFollowPlayer : MonoBehaviour
+{
+
+	[SerializeField]
+	Transform player = default;
+
+	[SerializeField, Range(1f, 20f)]
+	float distance = 5f;
+
+	[SerializeField, Min(0f)]
+	float focusRadius = 1f;
+
+	[SerializeField, Range(0f, 1f)] //higher value = faster focusing
+	float focusCentering = 0.5f;
+
+	Vector3 focusPoint;
+
+	void Awake()
+	{
+		focusPoint = player.position;
+	}
+
+	void LateUpdate()
+	{
+		UpdateFocusPoint();
+		Vector3 lookDirection = transform.forward;
+		Vector3 lookPosition = focusPoint - lookDirection * distance;
+
+		if (Physics.Raycast(
+			focusPoint, -lookDirection, out RaycastHit hit, distance
+		))
+		{
+			//TODO: How to handle when an object is in between camera and player
+			//lookPosition = focusPoint - lookDirection * hit.distance;
+		}
+
+		transform.localPosition = lookPosition;
+
+	}
+
+	/* If distance between the target and current focus points is greater than
+     * the radius, pull the focus toward the target, using (1−c)^t as the
+     * interpolator
+     */
+	void UpdateFocusPoint()
+	{
+		Vector3 targetPoint = player.position;
+		if (focusRadius > 0f)
+		{
+			float distance = Vector3.Distance(targetPoint, focusPoint);
+			if (distance > focusRadius)
+			{
+				focusPoint = Vector3.Lerp(
+					targetPoint, focusPoint, focusRadius / distance
+				);
+			}
+			if (distance > 0.01f && focusCentering > 0f)
+			{
+				focusPoint = Vector3.Lerp(
+					targetPoint, focusPoint,
+					Mathf.Pow(1f - focusCentering, Time.unscaledDeltaTime)
+				);
+			}
+		}
+		else
+		{
+			focusPoint = targetPoint;
+		}
+	}
+
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+
+    [SerializeField, Range(0f, 100f)]
+    float maxSpeed = 10f;
+
+    [SerializeField, Range(0f, 100f)]
+    float minSpeed = 5.0f;
+
+    [SerializeField, Range(0f, 100f)]
+    float jumpSpeed = 20.0f;
+
+    [SerializeField, Range(0f, 100f)]
+    float maxAcceleration = 14f;
+
+    Rigidbody body;
+    Vector3 velocity, desiredVelocity, lastPosition;
+
+
+    bool grounded;
+    float distToGround;
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        CapsuleCollider collider = GetComponent<CapsuleCollider>();
+        grounded = true;
+        distToGround = collider.bounds.extents.y + 0.01f;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        //playerInput = Vector2.ClampMagnitude(playerInput, 1f);
+        desiredVelocity = new Vector3(playerInput.x, 0f, 0f) * maxSpeed;
+
+        //Jump!
+        if (playerInput.y > 0 && CheckGrounded())
+        {
+            velocity = body.velocity;
+            velocity.y = jumpSpeed;
+            body.velocity = velocity;
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        velocity = body.velocity;
+        float maxSpeedChange = maxAcceleration * Time.deltaTime;
+        velocity.x = desiredVelocity.x;
+        body.velocity = velocity;
+    }
+
+
+    bool CheckGrounded() {
+        return Physics.Raycast(transform.position, -Vector3.up, distToGround);
+    }
+}
